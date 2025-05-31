ac6db0c1ab81aafdefb3433d40c95ac4ec77fbaf
faizan305513 <mailmuhammadfaizan@gmail.com>
2025-05-31 15:57:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('btn1').addEventListener('click', function () {
+  document.getElementById('emoji-display').textContent = '😀';
+});
+
+document.getElementById('btn2').addEventListener('click', function () {
+  document.getElementById('emoji-display').textContent = '😎';
+});
+
+document.getElementById('btn3').addEventListener('click', function () {
+  document.getElementById('emoji-display').textContent = '🎉';
+});
